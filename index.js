6c325c2930c8c3c089bc5d0f4a500c846d464f70
JodsonCadet <cadetjoddson@gmail.com>
2025-05-29 22:32:09 -0400

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1 = 10;
+console.log(code1)
+code1 = 7 + 3;
+console.log(code1)
+let code2 = 2 * 50 / 2 - 10;
+console.log(code2)
+let code3 = 79 % 40;
+console.log(code3)
+let message = "The vault has been secured. The combination is:"
+console.log(message)
+let codeA = `${code1} ${code2} ${code3}`
+console.log(codeA)
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(`${message} + ${codeB}`);
